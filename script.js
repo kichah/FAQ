ff938b95dfdbca7d585db30d590931430d1b58c3
kichah <abdellatifkichah@gmail.com>
2024-03-19 03:06:49 +0100

    finish

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-const stat_button = document.querySelectorAll('.faq__card--question-state');
-stat_button.forEach((btn) => {
-  btn.addEventListener('click', function () {
+const question = document.querySelectorAll('.faq__card--box');
+question.forEach((qt) => {
+  qt.addEventListener('click', function () {
     document
-      .querySelector(`.plus-${btn.dataset.answer}`)
+      .querySelector(`.plus-${qt.dataset.answer}`)
       .classList.toggle('hidden');
     document
-      .querySelector(`.minus-${btn.dataset.answer}`)
+      .querySelector(`.minus-${qt.dataset.answer}`)
       .classList.toggle('hidden');
     document
-      .querySelector(`.an-${btn.dataset.answer}`)
-      .classList.toggle('hidden');
-  });
-});
-const link = document.querySelectorAll('.faq__card--question-title');
-link.forEach((btn) => {
-  btn.addEventListener('click', function () {
-    document
-      .querySelector(`.plus-${btn.dataset.answer}`)
-      .classList.toggle('hidden');
-    document
-      .querySelector(`.minus-${btn.dataset.answer}`)
-      .classList.toggle('hidden');
-    document
-      .querySelector(`.an-${btn.dataset.answer}`)
+      .querySelector(`.an-${qt.dataset.answer}`)
       .classList.toggle('hidden');
   });
 });
